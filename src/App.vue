89c0084f5d44b107ade4c1a30cb265a98528b4d1
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-10-10 16:45:12 -0400

    init app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section id="WasThisPageHelpful" class="bg-white py-5" aria-describedby="WasThisPageHelpfulHeading">
+    <div class="container">
+      <form action="https://www.formstack.com/forms/index.php" target="_blank" method="post" enctype="multipart/form-data" class="text-center">
+        <!--  -->
+        <input type="hidden" name="form" value="2443653" />
+        <input type="hidden" name="viewkey" value="cePLhPMPWT" />
+        <input type="hidden" name="hidden_fields" id="hidden_fields2443653" value="" />
+        <input type="hidden" name="_submit" value="1" />
+        <input type="hidden" name="incomplete" id="incomplete2443653" value="" />
+        <input type="hidden" name="incomplete_password" id="incomplete_password2443653" />
+        <input type="hidden" name="style_version" value="3" />
+        <input type="hidden" id="viewparam" name="viewparam" value="685268" />
+
+        <h5 class="font-weight-bold" id="WasThisPageHelpfulHeading">
+          Was this page helpful?
+        </h5>
+
+        <div class="form-check form-check-inline">
+          <input v-model="wasItHelpful" class="form-check-input" type="radio" name="field44813009" id="field44813009_1" value="Yes" required>
+          <label class="form-check-label" for="inlineRadio1">Yes</label>
+        </div>
+
+        <div class="form-check form-check-inline">
+          <input v-model="wasItHelpful" class="form-check-input" type="radio" name="field44813009" id="field44813009_2" value="No" required>
+          <label class="form-check-label" for="inlineRadio2">No</label>
+        </div>
+
+        <div v-if="showComments" class="form-group">
+          <hr>
+          <label for="field44813218" class="font-weight-bold">Your Feedback:</label>
+          <textarea v-model="comments" class="form-control" id="field44813218" name="field44813218"></textarea>
+          <div class="small form-text text-muted">
+            <p>
+              Your feedback will be used to improve the County’s website. This form is not for emergencies or individual requests. To report problems or make service requests visit the <a href="https://service.hillsboroughcounty.org/311/" target="_blank">Customer Service Center's website</a>.
+            </p>
+            <p>
+              All information submitted by this form or via email becomes a public record to the extent provided by law.
+            </p>
+          </div>
+        </div>
+
+        <input v-model="page" type="hidden" id="field44813452" name="field44813452">
+
+        <div class="form-group mt-3">
+          <input type="submit" class="btn btn-outline-primary" value="Submit Feedback">
+        </div>
+
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      wasItHelpful: null,
+      comments: null,
+      page: window.location.href
+    }
+  },
+  computed: {
+    showComments() {
+      return (this.wasItHelpful == "No")
     }
   }
 }
